691c6e047656dbc54d07cf6fa4d1ac54ecee2f50
heyvishusri <vishwaskumar55347@gmail.com>
2024-09-09 16:09:11 +0530

    Update men_categ.dart

--- lib/categories/men_categ.dart
+++ lib/categories/men_categ.dart
@@ -21,54 +21,63 @@ class MenCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.all(30.0),
-          child: Text(
-            'Men',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.5,
-            ),
-          ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.68,
-          child: GridView.count(
-            mainAxisSpacing: 70,
-            crossAxisSpacing: 15,
-            crossAxisCount: 3,
-            children: List.generate(men.length, (index) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SubCategProducts(
-                                maincategName: 'men',
-                                subcategname: men[index],
-                              )));
-                },
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 70,
-                      width: 70,
-                      child: Image(
-                        image: AssetImage('images/men/men$index.jpg'),
-                      ),
-                    ),
-                    Text(men[index])
-                  ],
+    return Positioned(
+      bottom: 0,
+      
+
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height *0.8,
+        width:  MediaQuery.of(context).size.width*0.75,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                'Men',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
                 ),
-              );
-            }),
-          ),
-        )
-      ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.68,
+              child: GridView.count(
+                mainAxisSpacing: 70,
+                crossAxisSpacing: 15,
+                crossAxisCount: 3,
+                children: List.generate(men.length, (index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SubCategProducts(
+                                    maincategName: 'men',
+                                    subcategname: men[index],
+                                  )));
+                    },
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 70,
+                          width: 70,
+                          child: Image(
+                            image: AssetImage('images/men/men$index.jpg'),
+                          ),
+                        ),
+                        Text(men[index])
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
