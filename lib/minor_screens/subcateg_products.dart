56ac30593d804ede06bd749f9c4cd1bad90d5be3
heyvishusri <vishwaskumar55347@gmail.com>
2024-09-23 01:48:44 +0530

    Update subcateg_products.dart

--- lib/minor_screens/subcateg_products.dart
+++ lib/minor_screens/subcateg_products.dart
@@ -2,33 +2,35 @@ import 'package:flutter/material.dart';
 
 class SubCategProducts extends StatelessWidget {
   final String maincategName;
-  final String subcategname;
+  final String subcategName;
+
   const SubCategProducts(
-      {Key? key, required this.subcategname, required this.maincategName})
+      {Key? key, required this.subcategName, required this.maincategName})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back_ios_new,
-              color: Colors.black,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.black,
           ),
-          title: Text(
-            subcategname,
-            style: const TextStyle(color: Colors.black),
-          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-        body: Center(
-          child: Text(maincategName),
-        ));
+        title: Text(
+          subcategName,
+          style: const TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Center(
+        child: Text(maincategName),
+      ),
+    );
   }
 }
